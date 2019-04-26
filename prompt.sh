03910faed96f70eae6c5547f4d25c50d81873e30
Chris Paterson <cpaterson@conversantmedia.com>
2019-04-26 15:31:05 -0700

    changing git-dirty emoji

--- prompt.sh
+++ prompt.sh
@@ -36,7 +36,7 @@ find_git_branch() {
 find_git_dirty() {
   local status=$(git status --porcelain 2> /dev/null)
   if [[ "$status" != "" ]]; then
-    git_dirty="🔥"
+    git_dirty="💥"
   else
     git_dirty=''
   fi
